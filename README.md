54047f2391faa4c6ed0085649fc48262d25cffa2
icomefromthenet <admin@icomefromthenet.com>
2016-01-26 12:25:33 +1100

    continued work on entity unit tests

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # PointsMachine
-Library to build Points Systems.
+Library to build Points Systems for MySql.
+
+#Overview
+PointsMachine allows user defined rule systems that can be configured though values stored in database. 
+
+PointsMachine does not provide the GUI but is a set of classes that will manage the processing and storage of rules and scores.
+
+A points run starts with a selection of scores that are then applied to a formula defined as a series of rule groups chain together with each group containg one to many adjustment rules that either modify or multiply the base score. These scorce can then be rounded and capped.  
+
+
+#Concepts Overview:
+
+##Period of Validity.
+Many of the entities (Rules,Scores,Chains) all have a period of applicability. This library does not allow for future data only current. For example a delete operation will cause an existing entity to be closed meaning the databse record is not removed only its valid date range changed. If new entity is added it is active from that date. 
+
+##Systems and Zones
+All Formulas and Rules are linked to a System if you are running multiple reward schemes they would each belong to a different system. 
+
+A System has Zones these zones should be mutually exclusive to each other for example sales territories. These zones are used to further filter which rules should apply to a score. 
+
+
+## Score and Score Groups
+A Score is a starting point each score has a value. This value will be modified by the processor.
+
+You may have many scores in calculation run each score is modified independently. 
+
+Each Score may belong to a single Score Group. 
+
+Each product in your catelog could be assigned its own score with product categories making up the score groups.
+
+## Adjustment Rules and Adjustment Groups
+An adjustment rule contains a modifier and a multipler they can be:
+1. Linked to specific system zones.
+2. Linked to only a single Adjustment Group.
+
+A rule multipler is a multiplication operation.
+A rule modifier is a addition operation.
+
+
+An Adjustment Group is a container for one or more Adjustment Rules.
+
+A group has the following functions.
+1. Be linked to only apply on specific Score Groups (different groups for each product category)
+2. Be linked to one more Points Systems. (reuse one group across multiple point systems)
+3. Have a maximum and minimum multipler and modifier (based on combined value of adjustment rules applied in the run)
+4. Group can me made to be mandatory (always apply its rules)
+5. Limit on number of adjustment rules that are applied to score (limit to a single rule).
+6. Change how the linked rules are ordered (max or min).
+
+For example you can have an adjustment group apply only a single largest rule from those included in a calculation run.
+
+
+
+> If you developing a system and do not need the end user to configure the values through a GUI I would recommend that avoid using a library like mine.
+
+
+
--- src/IComeFromTheNet/PointsMachine/DB/Entity/AdjustmentGroup.php
+++ src/IComeFromTheNet/PointsMachine/DB/Entity/AdjustmentGroup.php
@@ -15,6 +15,39 @@ use IComeFromTheNet\PointsMachine\PointsMachineException;
 class AdjustmentGroup extends CommonEntity implements ActiveRecordInterface
 {
     
+      
+    protected $aValidation = [
+       'integer' => [
+            ['episode_id'],['max_count']
+        ]
+        ,'boolean' => [
+            ['is_mandatory']
+        ]
+        ,'lengthBetween' => [
+            ['group_name','1','100'],['group_name_slug','1','100']
+        ]
+        ,'slug' => [
+            ['group_name_slug']
+        ]
+        ,'required' => [
+            ['adj_group_id','group_name','group_name_slug','enabled_from','enabled_to']
+        ]
+        ,'instanceOf' => [
+            ['enabled_from','DateTime'],['enabled_to','DateTime']
+        ]
+        ,'min' => [
+           ['episode_id',1],['max_count',1],['order_method',0]
+        ]
+        ,'max' => [
+           ['episode_id',4294967295],['max_count',1000],['order_method',1]
+        ]
+        ,'numeric' => [
+            ['max_modifier'],['min_modifier'],['max_multiplier'],['min_multiplier']
+        ]
+        
+    ];
+   
+    
     public $iEpisodeID;
     
     public $sAdjustmentGroupID;
@@ -43,41 +76,232 @@ class AdjustmentGroup extends CommonEntity implements ActiveRecordInterface
     
     
     
-    public function saveEpisode(DateTime $oProcessDte)
+    //--------------------------------------------------------------------------
+    # Entity Hooks
+   
+    
+    protected function checkRemoveTemportalFK($aDatabaseData)
     {
+        $oGateway               = $this->getTableGateway();
+        $oRuleGroupLimitGateway = $oGateway->getGatewayCollection()->getGateway('pt_rule_group_limits');
+        $oRuleGatway            =  $oGateway->getGatewayCollection()->getGateway('pt_rule'); 
+        $oChainMemberGateway    =  $oGateway->getGatewayCollection()->getGateway('pt_chain_member'); 
+        
+        $bResult           = array();
+        $sChainMemberId    = $aDatabaseData['rule_chain_id'];
+        
+        
+        if(true === $oChainMemberGateway->checkParentChainRequired($sChainMemberId)) {
+            $bResult['RuleChainMember'] = true;
+        } 
+        
+        return $bResult;
+    }
+    
+    
+    protected function createNewEntity($aDatabaseData)
+    {
+        $bSuccess          = false;
+        $oGateway          = $this->getTableGateway();
+        
+        $bSuccess = $oGateway->insertQuery()
+         ->start()
+            ->addColumn('rule_group_id',        $aDatabaseData['rule_group_id'])
+            ->addColumn('rule_group_name',      $aDatabaseData['rule_group_name'])
+            ->addColumn('rule_group_name_slug', $aDatabaseData['rule_group_name_slug'])
+            ->addColumn('max_multiplier',       $aDatabaseData['max_multiplier'])
+            ->addColumn('min_multiplier',       $aDatabaseData['min_multiplier'])
+            ->addColumn('max_modifier',         $aDatabaseData['max_modifier'])
+            ->addColumn('min_modifier',         $aDatabaseData['min_modifier'])
+            ->addColumn('max_count',            $aDatabaseData['max_count'])
+            ->addColumn('order_method',         $aDatabaseData['order_method'])
+            ->addColumn('is_mandatory',         $aDatabaseData['is_mandatory'])
+            ->addColumn('enabled_from',         $aDatabaseData['enabled_from'])
+            ->addColumn('enabled_to',           $aDatabaseData['enabled_to'])
+         ->end()
+        ->insert(); 
+
+        if($bSuccess) {
+                
+                $this->aLastResult['result'] = true;
+                $this->aLastResult['msg']    = 'Created new Adjustment Group Episode';
+                $this->iEpisodeID            =  (int) $oGateway->lastInsertId();
+                     
+        } else {
+            
+            $this->aLastResult['result'] = false;
+            $this->aLastResult['msg']    = 'Unable to insert AdjustmentGroup Episode.';
+            
+        }
+
+
+       return $bSuccess;
+    }
+    
+    protected function createNewEpisode($aDatabaseData)
+    {
+        $bSuccess          = false;
+        $oGateway          = $this->getTableGateway();
+     
+        $oNow = $oGateway->getNow();
+        $aDatabaseData['enabled_to'] = $oNow;
+      
+            
+        if(true === $this->closeEpisode($aDatabaseData)) {
+        
+            $aDatabaseData['enabled_from']  = $oNow;
+            $aDatabaseData['enabled_to']    = new DateTime('3000-01-01');  
+            
+            # up to the caller to rollback
+            if(false === $this->createNewEntity($aDatabaseData)) {
+                throw new PointsMachineException($this->aLastResult['msg']);
+            }
+            
+        } else {
+            
+            $this->aLastResult['result'] = false;
+            $this->aLastResult['msg']    = 'Unable to create new AdjustmentGroup episode as unable to close the earlier episode';
+            
+        }
+        
+        
+        return $this->aLastResult['result'];
+    }
+    
+    
+    protected function updateExistingEpisode($aDatabaseData)
+    {
+        $bSuccess          = false;
+        $oGateway          = $this->getTableGateway();
        
+        # new episode
+        
+        $bSuccess = $oGateway->updateQuery()
+            ->start()
+                ->addColumn('rule_group_name',      $aDatabaseData['rule_group_name'])
+                ->addColumn('rule_group_name_slug', $aDatabaseData['rule_group_name_slug'])
+                ->addColumn('max_multiplier',       $aDatabaseData['max_multiplier'])
+                ->addColumn('min_multiplier',       $aDatabaseData['min_multiplier'])
+                ->addColumn('max_modifier',         $aDatabaseData['max_modifier'])
+                ->addColumn('min_modifier',         $aDatabaseData['min_modifier'])
+                ->addColumn('max_count',            $aDatabaseData['max_count'])
+                ->addColumn('order_method',         $aDatabaseData['order_method'])
+                ->addColumn('is_mandatory',         $aDatabaseData['is_mandatory'])
+            ->where()
+                ->filterByEpisode($aDatabaseData['episode_id'])
+                ->filterByAdjustmentGroup($aDatabaseData['rule_group_id'])
+             ->end()
+           ->update(); 
+           
+    
+         if($bSuccess) {
+            $this->aLastResult['result'] = true;
+            $this->aLastResult['msg']    = 'Updated existing AdjustmentGroup Episode';
+            
+        } else {
+            $this->aLastResult['result'] = false;
+            $this->aLastResult['msg']    = 'Unable to update existing AdjustmentGroup Episode';
+        }
+        
+ 
+       return $bSuccess;
+        
+        
     }
     
-    public function deleteEpisode(DateTime $oProcessDte)
+    
+    protected function closeEpisode($aDatabaseData)
     {
-       
+        $oGateway              = $this->getTableGateway();
+        
+        $bSuccess = $oGateway->updateQuery()
+                             ->start()
+                                ->addColumn('enabled_to' , $aDatabaseData['enabled_to'])
+                              ->where()
+                                ->filterByEpisode($aDatabaseData['episode_id'])
+                                ->filterByAdjustmentGroup($aDatabaseData['rule_group_id'])
+                                ->filterByCurrent(new DateTime('3000-01-01'))
+                             ->end()
+                           ->update(); 
+
+        if($bSuccess) {
+            $this->aLastResult['result'] = true;
+            $this->aLastResult['msg']    = 'Closed this Adjustment Group episode';
+        } else {
+            $this->aLastResult['result'] = false;
+            $this->aLastResult['msg']    = 'Unable to close this Adjustment Group episode';
+        }
+        
+        return $bSuccess;
     }
     
-    public function validate(DateTime $oProcessDte)
+    //--------------------------------------------------------------------------
+    # Validation Hooks
+    
+    protected function getDataForValidation()
     {
-         
+        return array(
+             'episode_id'        => $this->iEpisodeID
+            ,'adj_group_id'      => $this->sAdjustmentGroupID
+            ,'group_name'        => $this->sGroupName
+            ,'group_name_slug'   => $this->sGroupNameSlug
+            ,'min_multiplier'    => $this->fMinMultiplier
+            ,'max_multiplier'    => $this->fMaxMultiplier
+            ,'min_modifier'      => $this->fMinModifier
+            ,'max_modifier'      => $this->fMaxModifier
+            ,'max_count'         => $this->iMaxCount
+            ,'order_method'      => $this->iOrderMethod
+            ,'is_mandatory'      => $this->bIsMandatory        
+            ,'enabled_from'      => $this->oEnabledFrom
+            ,'enabled_to'        => $this->oEnabledTo
+          
+        );
     }
     
-    public function validateNewEpisode(DateTime $oProcessDte)
+    
+    protected function validateNewEpisode()
     {
+        $aData = $this->getDataForValidation();
+        $aRules = $this->aValidation;
+        
+        // we need the episode if going to create new episode
+        array_push($aRules['required'],['episode_id']);
         
         
+        return $this->validate($aData,$aRules);
     }
-    
-    
-    public function validateNewEntity(DateTime $oProcessDte)
+   
+    protected function validateNew()
     {
+        $aData = $this->getDataForValidation();
+        $aRules = $this->aValidation;
         
-        
+        return $this->validate($aData,$aRules);
     }
     
-    
-    public function validateUpdate(DateTime $oProcessDte)
+    protected function validateUpdate()
     {
+        $aData = $this->getDataForValidation();
+        $aRules = $this->aValidation;
+        
+        // we need the episode if to do an update
+        array_push($aRules['required'],['episode_id']);
         
         
+        return $this->validate($aData,$aRules);
     }
-    
+          
+    protected function validateRemove()
+    {
+        $aData = $this->getDataForValidation();
+        $aRules = $this->aValidation;
+        
+        // we need the episode if to do an remove
+        array_push($aRules['required'],['episode_id']);
+        
+        return $this->validate($aData,$aRules);
+    }
+   
 }
 /* End of File */
 
